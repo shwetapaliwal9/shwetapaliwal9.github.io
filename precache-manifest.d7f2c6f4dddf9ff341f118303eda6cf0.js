4888ab3c538ff54c26e3fa787606bf3d4fc26502
shweta paliwal <shweta.paliwal1234@gmail.com>
2018-11-28 17:33:01 +0530

    restaurant finder app

---
+++ precache-manifest.d7f2c6f4dddf9ff341f118303eda6cf0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "66580dbf65016945d9d5",
+    "url": "/static/js/main.66580dbf.chunk.js"
+  },
+  {
+    "revision": "9574ba8228b3addbd598",
+    "url": "/static/js/1.9574ba82.chunk.js"
+  },
+  {
+    "revision": "66580dbf65016945d9d5",
+    "url": "/static/css/main.16aa5ea1.chunk.css"
+  },
+  {
+    "revision": "f183c33713d833a3856db46ff7907254",
+    "url": "/index.html"
+  }
+];
